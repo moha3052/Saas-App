c6a39fa91857b74caca5f430afcbd14c486aa9b5
Mohamed <moom0001@stud.kea.dk>
2025-06-30 11:21:44 +0200

    forside færdig

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,49 @@
-import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-        <Button className="ml-2">hello</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+        <section className="home-section">
+            <CompanionCard
+            id='123'
+            name='Neura the brainy Explorer'
+            topic="Neural Network of the Brain "
+            subject="Science"
+            duration={45}
+            color="#ffda6e"
+            />
+            <CompanionCard
+                id="543"
+                name='Countsy the Number Wizard'
+                topic="Derivatives & Integrals"
+                subject="Math"
+                duration={30}
+                color="#e5d0ff"
+            />
+            <CompanionCard
+                id="877"
+                name='Verba the Vocabulary Builder'
+                topic="English Literature"
+                subject="Language"
+                duration={30}
+                color="#BDE7FF"
+            />
+        </section>
+
+        <section className="home-section">
+            <CompanionList
+             title="Recently completed sessions"
+            companions={recentSessions}
+            className="w-2/3 max-lg:w-full"
+            />
+            <CTA/>
+        </section>
+    </main>
   )
 }
 
